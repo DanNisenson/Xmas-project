bd5ce3e983855a2aa25281da3e18df2dc0c218f2
DanNisenson <danpablo1@gmail.com>
2022-12-29 17:28:36 +0100

    abstacted idCheck to separate file

---
+++ middlewares/validation/idCheck.js
@@ -0,0 +1,14 @@
+// let superheroes = require("../../assets/superhero.json");
+
+const idCheck = (paramsId, superheroes) => {
+
+  let id = parseInt(paramsId);
+
+  if (superheroes.findIndex(hero => hero.id === id) === -1) {
+    return false;
+  } else {
+    return id;
+  }
+};
+
+module.exports = idCheck;
